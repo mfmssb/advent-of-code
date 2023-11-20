aa19fbefae17720579dbaa3e701c94c621f14fc1
Magne Furuholmen Myhren <mfm@ssb.no>
2023-11-20 16:42:08 +0100

    Setup for 2023
    ssb-project
    mappestruktur
    template for hver dag

---
+++ Advent of Code 2023/folder_template/solution.py
@@ -0,0 +1,29 @@
+filnavn = ["data0.txt", "data1.txt", "data2.txt"]
+data = []
+for fil in filnavn:
+    with open(fil, 'r') as file:
+        data.append(file.read())
+
+print("Størrelsen på inndata:")
+for x in data:
+    print(len(x))
+
+# # Oppgave 1
+
+# ## Mellomregning
+
+
+
+# ## Løsning
+
+
+
+# # Oppgave 2
+
+# ## Mellomregning
+
+
+
+# ## Løsning
+
+
---
+++ README-Copy1.md
@@ -0,0 +1,10 @@
+# advent-of-code
+
+
+
+Opprettet av:
+Magne Furuholmen Myhren <mfm@ssb.no>
+
+---
+
+Legg inn beskrivelse av prosjektet her.
