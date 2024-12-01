de3b9047eae3e980b94b1aff6e7bd803ca4ade20
ssb-mfm <mfm@ssb.no>
2024-12-01 18:07:26 +0000

    prep

---
+++ Advent of Code 2024/01 - Solved/solution.py
@@ -0,0 +1,31 @@
+filnavn = ["data0.txt", "data1.txt", "data2.txt"]
+data = []
+for fil in filnavn:
+    with open(fil, 'r') as file:
+        data.append(file.read())
+
+print("Størrelsen på inndata:")
+for i, x in enumerate(data):
+    print(i, len(x))
+
+# # Problem 1
+
+d = data[0].split("\n")
+
+# ## Intermediate Steps 1
+
+
+
+# ## Solution 1
+
+
+
+# # Problem 2
+
+# ## Intermediate Steps 1
+
+
+
+# ## Solution 2
+
+
