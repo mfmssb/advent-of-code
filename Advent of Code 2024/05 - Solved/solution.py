2917db0ec193578fb941433fcfa02ca8efbad9ba
ssb-mfm <mfm@ssb.no>
2024-12-05 12:07:57 +0000

    4 p2 solved

--- Advent of Code 2024/05 - Solved/solution.py
+++ Advent of Code 2024/05 - Solved/solution.py
@@ -55,14 +55,55 @@ ans1 = sum_mid_pages
 
 print(ans1)
 
-
-
 # # Problem 2
 
 # ## Intermediate Steps 2
 
+d = data[1].split("\n\n")
+rules = d[0].split("\n")
+updates = d[1].split("\n")
 
+
+def fix_order(update: str) -> str:
+    fixed_update_order = update
+
+    swapped = False
+    for rule in rules:
+        rule_passed = True
+        split = rule.split('|')
+        left = split[0]
+        right = split[1]
+
+        if (update.find(left) != -1) & (update.find(right) != -1):
+            update_split = fixed_update_order.split(',')
+            l_index = [i for i, x in enumerate(update_split) if update_split[i] == left][0]
+            r_index = [i for i, x in enumerate(update_split) if update_split[i] == right][0]
+
+            if l_index > r_index:
+                # Swap position
+                update_split[l_index], update_split[r_index] = update_split[r_index], update_split[l_index]
+                swapped = True
+            
+            fixed_update_order = (',').join(update_split)
+    return fixed_update_order, swapped
+
+
+fixed_updates = []
+for update in updates:
+    while fix_order(update)[1]:
+        update = fix_order(update)[0]
+    fixed_updates.append(update)
+    
+
+sum_mid_pages = 0
+for update in fixed_updates:
+    split = update.split(",")
+    mid_index = int(len(split) / 2)
+    sum_mid_pages += int(split[mid_index])
+ans2 = sum_mid_pages-ans1
 
 # ## Solution 2
+
+print(ans2)
 
 
