9e16c34c65dcbb74df90769ae248b4ff8d620b58
ssb-mfm <mfm@ssb.no>
2024-12-06 14:55:28 +0000

    6 p1 solved

--- Advent of Code 2024/06/solution.py
+++ Advent of Code 2024/06/solution.py
@@ -8,16 +8,66 @@ print("** Inndata størrelse **")
 for i, x in enumerate(data):
     print(f"data[{i}]: {len(x):8} tegn")
 
+
 # # Problem 1
 
-d = data[0].split("\n")
+def locate_guard(d: list, guard_chars=['^', '>', 'v', '<']) -> (int, int, str):
+    for y in range(H):
+        for x in range(W):
+            if d[y][x] in guard_chars:
+                return (x, y, d[y][x])
+    raise ValueError("Guard not found")
+
+
+def is_in_bounds(d: str, posx: int, posy: int) -> bool:
+    if (0 <= posx < W) and (0 <= posy < H):
+        return True
+    return False
+
+
+def is_markable(d, posx, posy, obstacle='#'):
+    if is_in_bounds(d, posx, posy) and d[posy][posx] != obstacle:
+        return True
+    return False
+
+
+def mark_map(d: list, posx: int, posy: int, mark='X', obstacle='#'):
+    if is_in_bounds(d, posx, posy):
+        if d[posy][posx] != obstacle:
+            line = d[posy]
+            line = line[:posx] + mark + line[(posx + 1):]
+            d[posy] = line
+    return d
+
+
+def take_step(d: list, guard: tuple, obstacle='#') -> (int, int, str):
+    gx, gy, gdir = guard
+
+    if is_markable(d, gx, gy):
+        gx_new, gy_new = gx+DIRS[gdir][0], gy+DIRS[gdir][1]
+        d_marked = mark_map(d, gx, gy)
+
+        if is_in_bounds(d, gx_new, gy_new):
+            if d[gy_new][gx_new] == obstacle:
+                gdir = ROTATE90[gdir]
+            else:
+                gx, gy = gx_new, gy_new
+        else:
+            gx, gy = gx_new, gy_new
+    else:
+        d_marked = d
+    
+    return (d_marked, (gx, gy, gdir))
+
+
+d = data[1].split("\n")
 
 # `d[y][x]` med origo øverst til venstre
 
 H = len(d)
 W = len(d[0])
 
-d
+mapd = d[::]
 
 DIRS = {
     '^': ( 0, -1),
@@ -32,45 +82,20 @@ ROTATE90 = {
     '<': '^',
 }
 
+g = locate_guard(mapd)
 
-def locate_guard(d: list, guard_chars=['^', '>', 'v', '<']) -> (int, int, str):
-    for y in range(H):
-        for x in range(W):
-            if d[y][x] in guard_chars:
-                return (x, y, d[y][x])
-    raise ValueError("Guard not found")
+while is_in_bounds(mapd, g[0], g[1]):
+    mapd, g = take_step(mapd, g)
 
+count = 0
+for line in mapd:
+    count += line.count("X")
+ans1 = count
 
-def is_in_bounds(d: str, posx: int, posy: int) -> bool:
-    if (0 <= posx < W) and (0 <= posy < H):
-        return True
-    return False
-is_in_bounds(d, -1, 0)
-
-
-def mark_map(d: list, posx: int, posy: int, mark='X'):
-    if is_in_bounds(d, posx, posy):
-        line = d[posy]
-        line = line[:posx] + mark + line[(posx + 1):]
-        d[posy] = line
-    return d
-
-
-mark_map(d, 9, 9)
-
-
-# +
-def take_step(d: list, guard: tuple) -> (int, int, str):
-    gx, gy, gdir = guard
-    
-    
-# -
-
-locate_guard(d)
 
 # ## Intermediate Steps 1
 
-
+print(ans1)
 
 # ## Solution 1
 
