46da79c98e697adb51f9aa3d087cc037c0428080
ssb-mfm <mfm@ssb.no>
2025-12-03 19:55:30 +0100

    Day 3

--- aoc_2025/03/main3.py
+++ aoc_2025/03/main3.py
@@ -1,6 +1,3 @@
-from itertools import combinations
-
-
 def parse_data(data_path: str) -> list:
     with open(data_path, 'r') as file:
         content = file.read()
@@ -22,17 +19,11 @@ def p1(data: list) -> int:
     return sum_joltages
 
 
-# +
-data = parse_data("data/data0.txt")
-
-p1(data)
-
-
-# -
-
+from itertools import combinations
 def p2_too_slow(data:list) -> int:
     """
-    This code should provide the solution, but it is too slow as i inspects all combinations in the puzzle input
+    This code should provide the solution, but it is too slow as i inspects all combinations in the puzzle input.
+    The search goes on to find a more optimal sollution
     """
     sum_joltages = 0
     for bank in data:
@@ -48,52 +39,64 @@ def p2_too_slow(data:list) -> int:
 
 
 def recursive_check_of_remaining_bank(max_bank, remaining_bank, num_remaining):
+    num_remaining -= 1
     if num_remaining == 0:
-        return max_bank
+        return max_bank + max(remaining_bank)
+    
+    if len(remaining_bank) == num_remaining:
+        return max_bank + remaining_bank
     
     stripped_bank = remaining_bank[:-num_remaining]
+
     max_num = max(stripped_bank)
     max_index = stripped_bank.find(max_num)
     
     new_remaining_bank = remaining_bank[(max_index + 1):]
-
-    if new_remaining_bank == remaining_bank:
-        return max_bank + remaining_bank
     max_bank += max_num
     
-    # print(f"{max_bank}, {new_remaining_bank}, {num_remaining}", )
-    
-    new_max_bank = recursive_check_of_remaining_bank(max_bank, new_remaining_bank, num_remaining - 1)
+    new_max_bank = recursive_check_of_remaining_bank(max_bank, new_remaining_bank, num_remaining)
     return new_max_bank
 
 
-test = recursive_check_of_remaining_bank("", "818181911112111", 12)
-print(test)
-test == "888911112111"
+def p2(data, num_batteries_to_turn):
+    sum_joltages = 0
+    for bank in data:
+        max_bank = recursive_check_of_remaining_bank("", bank, num_batteries_to_turn)
+        sum_joltages += int(max_bank)
+    return sum_joltages
 
-test
 
-test == "987654321111"
+data = parse_data("data/data1.txt")
 
-len(test)
+# %timeit p1(data)
 
-len("234234234234278")
+# %timeit p2(data, 2)
 
-len("234234234278")
+# %timeit p2(data, 12)
 
-"234234234278"[-11]
 
-remaining_bank = '819181911112111'
 
-num_remaining = 6
+# +
+### TEST SUITE
+tests = [
+    "987654321111111",
+    "811111111111119",
+    "234234234234278",
+    "818181911112111"
+]
+answers = [
+    "987654321111",
+    "811111111119",
+    "434234234278",
+    "888911112111"
+]
 
-stripped_bank = remaining_bank[:-num_remaining]
-
-max_num = max(stripped_bank)
-max_index = stripped_bank.find(max_num)
-
-new_remaining_bank = remaining_bank[(max_index + 1):]
-
-new_remaining_bank
-
-p2(data)
+for (test, answer) in zip(tests, answers):
+    try:
+        proposal = recursive_check_of_remaining_bank("", test, 12)
+        print(test,
+              proposal,
+              answer, "👌" if answer == proposal else "❌",
+              len(proposal))
+    except:
+        print(test, answer, "⚠️")
